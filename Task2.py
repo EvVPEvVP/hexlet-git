56a0b5ea9663f2b6f3adcd0d7030fb6809080af7
Evgeniy P <genepastukhov@gmail.com>
2022-11-05 18:00:21 +0300

    New Task2

--- Task2.py
+++ Task2.py
@@ -3,7 +3,7 @@ def odometer(oksana: list) -> int:
     distance = 0
     for i in range(len(oksana)):
         if i == 0:
-            distance += oksana[i]
+            distance += oksana[i] * oksana[i+1]
         elif i > 0 and i % 2 == 0:
             distance += oksana[i] * (oksana[i+1]-oksana[i-1])
     return distance

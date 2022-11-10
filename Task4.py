53127049f7e7920b2da90e68e33f403c7c269676
Evgeniy P <genepastukhov@gmail.com>
2022-11-10 10:08:22 +0300

    New Task4.py

--- Task4.py
+++ Task4.py
@@ -1,23 +1,24 @@
 def  MadMax(N: int, Tele: list) -> list:
 
+    check = True
+    while (check):
+        check = False
+        for i in range(N - 1):
+            if Tele[i] > Tele[i + 1]:
+                Tele[i], Tele[i + 1] = Tele[i + 1], Tele[i]
+                check = True
+
     maks = max(Tele)
     maxindex = Tele.index(maks)
     middleindex = N // 2
 
-    Tele[middleindex] ,Tele[maxindex] = Tele[maxindex] ,Tele[middleindex]
+    Tele[middleindex], Tele[maxindex] = Tele[maxindex], Tele[middleindex]
 
     check = True
-
-    while(check):
+    while (check):
         check = False
-
-        for i in range(middleindex):
-            if Tele[i] > Tele[i + 1]:
-                Tele[i] ,Tele[i + 1] = Tele[i + 1] ,Tele[i]
-                check = True
-
-        for i in range(middleindex , len(Tele) - 1):
+        for i in range(middleindex, len(Tele) - 1):
             if Tele[i] < Tele[i + 1]:
-                Tele[i] ,Tele[i + 1] = Tele[i + 1] ,Tele[i]
+                Tele[i], Tele[i + 1] = Tele[i + 1], Tele[i]
                 check = True
     return Tele

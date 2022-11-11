33a095433044ae05e78dab01bec58570fb5d5bef
Evgeniy P <genepastukhov@gmail.com>
2022-11-11 09:57:52 +0300

    New Task6.py

--- Task6.py
+++ Task6.py
@@ -15,22 +15,22 @@ def PatternUnlock(N: int,hits: list) -> str:
                     next = hits1[hits1.index(k) + 1]
                     if k == 6 or k == 9 or k == 4 or k == 7:
                         if next == 2:
-                            sum += 1.41421
+                            sum += 1.414215
                         else:
                             sum += 1.00000
                     elif k == 1 or k == 3:
                         if next == 5 or next == 8:
-                            sum += 1.41421
+                            sum += 1.414215
                         else:
                             sum += 1.00000
                     elif k == 5 or k == 8:
                         if next == 1 or next == 3:
-                            sum += 1.41421
+                            sum += 1.414215
                         else:
                             sum += 1.00000
                     else:
                         if next == 6 or next == 9 or next == 4 or next == 7:
-                            sum += 1.41421
+                            sum += 1.414215
                         else:
                             sum += 1.00000
         hits1.pop(0)

926d4bf21b7b2b37e8ef759bcc5f4ba6c81a0d9e
Evgeniy P <genepastukhov@gmail.com>
2022-11-14 12:02:00 +0300

    New Task7.py

--- Task7.py
+++ Task7.py
@@ -1,46 +1,8 @@
+import textwrap
+
 def WordSearch(n: int,s: str, subs: str) -> list:
 
-    s1 = list(s)
-    length = 0
-    length1 = 0
-    x = ''
-    x1 = ''
-    count = 0
-    m = False
-
-    if len(s) == n:
-        x1 = s
-        m = True
-
-    for i in s1:
-        if m == True:
-            break
-        count += 1
-        x += i
-        length += 1
-        length1 += 1
-        if i == ' ' and length <= n:
-            x1 += x
-            x = ''
-            length1 = 0
-        elif length > (n+1) and length1 < n:
-            x1 += '\n'
-            length = len(x)
-            length1 = len(x)
-        elif length == n and s1[count] == ' ':
-            x1 += x
-            x1 += '\n'
-            x = ''
-            s1.pop(count)
-            length = len(x)
-            length1 = len(x)
-        elif length1 >= n:
-            x1 += x
-            x = ''
-            length1 = 0
-        elif count == len(s1):
-            x1 += x
-
+    x1 = textwrap.fill(s,n)
     x2 = x1.split('\n')
     subs1 = ' ' + subs + ' '
     result = []

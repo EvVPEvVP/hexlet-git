98e0ee8a3f2d0f1e08131c9272133bec4917850a
Evgeniy P <genepastukhov@gmail.com>
2022-11-20 10:49:19 +0300

    Task14.py

--- Task14.py
+++ Task14.py
@@ -1,13 +1,26 @@
 def Unmanned(L: int, N: int, track: list) -> int:
 
-    if N == 1:
+    if N == 1 and len(track) == 3:
         result = track[0]
-    if N == 1 and (result % (track[1] + track[2]) >= track[1]):
+    if N == 1 and len(track) == 1:
+        result = track[0][0]
+    if N == 1 and result >= L:
+        return L
+    if N == 1 and len(track) == 3 and (result % (track[1] + track[2]) >= track[1]):
         result += L - track[0]
         return result
-    if N == 1 and (result % (track[1] + track[2]) < track[1]):
+    if N == 1 and len(track) == 3 and (result % (track[1] + track[2]) < track[1]):
         result += (track[1] - (result % (track[1] + track[2])))
         result += L - track[0]
+        return result
+
+    if N == 1 and len(track) == 1 and (result % (track[0][1] + track[0][2]) >= track[0][1]):
+        result += (track[0][1] - (result % (track[0][1] + track[0][2])))
+        result += L - track[0][0]
+        return result
+    if N == 1 and len(track) == 1 and (result % (track[0][1] + track[0][2]) < track[0][1]):
+        result += (track[0][1] - (result % (track[0][1] + track[0][2])))
+        result += L - track[0][0]
         return result
 
     result = track[0][0]

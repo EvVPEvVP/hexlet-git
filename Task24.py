f666904ce532e5e40c9d8dbd1a24f9536d1928be
Evgeniy P <genepastukhov@gmail.com>
2022-11-29 23:28:40 +0300

    New Task24.py

--- Task24.py
+++ Task24.py
@@ -8,7 +8,6 @@ def MatrixTurn(mat: list, M: int, N: int, T: int):
             result1.append(int(k1))
         mat2.append(result1)
 
-    mat = mat2.copy()
     del result1
     del x
 
@@ -19,44 +18,40 @@ def MatrixTurn(mat: list, M: int, N: int, T: int):
         right = N - 1
         while left < right and top < bottom:
 
-            prev = mat[top + 1][left]
+            prev = mat2[top + 1][left]
             for i in range(left, right + 1):
-                curr = mat[top][i]
-                mat[top][i] = prev
+                curr = mat2[top][i]
+                mat2[top][i] = prev
                 prev = curr
 
             top += 1
 
             for i in range(top, bottom + 1):
-                curr = mat[i][right]
-                mat[i][right] = prev
+                curr = mat2[i][right]
+                mat2[i][right] = prev
                 prev = curr
 
             right -= 1
 
             for i in range(right, left - 1, -1):
-                curr = mat[bottom][i]
-                mat[bottom][i] = prev
+                curr = mat2[bottom][i]
+                mat2[bottom][i] = prev
                 prev = curr
 
             bottom -= 1
 
             for i in range(bottom, top - 1, -1):
-                curr = mat[i][left]
-                mat[i][left] = prev
+                curr = mat2[i][left]
+                mat2[i][left] = prev
                 prev = curr
 
             left += 1
 
-    matrix1 = []
-    for i in range(len(mat)):
+    for i in range(len(mat2)):
         matrix2 = ''
-        for j in range(len(mat[i])):
-            matrix2 += str(mat[i][j])
-        matrix1.append(matrix2)
-
-    mat = matrix1.copy()
-
+        for j in mat2[i]:
+            matrix2 += str(j)
+        mat[i] = matrix2
 
 
 

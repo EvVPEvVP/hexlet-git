0f9e7c2f91e8c322ccd50dd582668081cf152e7f
Evgeniy P <genepastukhov@gmail.com>
2022-12-29 12:09:04 +0300

    algotask3.py

--- Algotask3.py
+++ Algotask3.py
@@ -8,18 +8,18 @@ class DynArray:
         self.capacity = 16
         self.array = self.make_array(self.capacity)
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.count
 
-    def make_array(self, new_capacity):
+    def make_array(self, new_capacity: int):
         return (new_capacity * ctypes.py_object)()
 
-    def __getitem__(self, i):
+    def __getitem__(self, i: int):
         if i < 0 or i >= self.count:
             raise IndexError('Index is out of bounds')
         return self.array[i]
 
-    def resize(self, new_capacity):
+    def resize(self, new_capacity: int):
         new_array = self.make_array(new_capacity)
         for i in range(self.count):
             new_array[i] = self.array[i]
@@ -32,7 +32,7 @@ class DynArray:
         self.array[self.count] = itm
         self.count += 1
 
-    def insert(self, i, itm):
+    def insert(self, i: int, itm):
         if i > 0 and i > self.count:
             raise IndexError('Index is out of bounds')
         if self.count == self.capacity:
@@ -42,7 +42,7 @@ class DynArray:
         self.array[i] = itm
         self.count += 1
 
-    def delete(self, i):
+    def delete(self, i: int):
         if self.count == 0:
             return None
         self.__getitem__(i)
@@ -58,3 +58,4 @@ class DynArray:
             self.resize((int(self.capacity/1.5)))
         elif (self.count/self.capacity) < 0.5 and int(self.capacity/1.5) <= 16:
             self.resize(16)
+
--- test_algotask3.py
+++ test_algotask3.py
@@ -27,41 +27,50 @@ class MyTestCase(unittest.TestCase):
         for i in range(1, 5):
             self.da.append(i)
         try:
-            self.da.insert(6,10)
-        except Exception:
+            self.da.insert(6, 10)
+        except IndexError:
             print('out of the range')
 
     def test_delete1(self):
         for i in range(1, 5):
             self.da.append(i)
         self.da.delete(1)
-        self.assertEqual(self.da.count,3)
+        self.assertEqual(self.da.count, 3)
         self.assertTrue(self.da.capacity == 16)
 
     def test_delete2(self):
         for i in range(1, 34):
             self.da.append(i)
         self.da.delete(1)
-        self.assertEqual(self.da.count,32)
+        self.assertEqual(self.da.count, 32)
         self.assertTrue(self.da.capacity == 64)
 
     def test_delete3(self):
-        for i in range(1, 14):
+        for i in range(1, 13):
             self.da.append(i)
         self.da.resize(24)
         self.assertTrue(self.da.capacity == 24)
         self.da.delete(1)
-        self.assertEqual(self.da.count,12)
-        # self.assertTrue(self.da.capacity == int((22/1.5)))
-        self.assertTrue(self.da.capacity == 24)
+        self.assertEqual(self.da.count, 11)
+        self.assertTrue(self.da.capacity == int(24/1.5))
 
     def test_delete4(self):
         for i in range(1, 5):
             self.da.append(i)
         try:
             self.da.delete(-1)
-        except Exception:
+        except IndexError:
             print('out of the range')
+
+    # def test_delete5(self):
+    #     for i in range(1, 2):
+    #         self.da.append(i)
+    #     self.da.resize(32)
+    #     self.assertTrue(self.da.capacity == 32)
+    #     self.da.delete(0)
+    #     self.assertEqual(self.da.count, 0)
+    #     self.assertTrue(self.da.capacity == int((32/1.5)))
+
 
 
 if __name__ == '__main__':

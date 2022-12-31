0872bbf24927dc90912aa7e30a05df5320548bf2
Evgeniy P <genepastukhov@gmail.com>
2022-12-31 16:11:53 +0300

    lastversion

--- queuetwostacks.py
+++ queuetwostacks.py
@@ -4,24 +4,23 @@ class Queue:
         self.s1 = []
         self.s2 = []
 
+    def enqueue(self, item):
+        self.s1.append(item)
+
     def size1(self):
         return len(self.s1)
 
     def size2(self):
         return len(self.s2)
 
-    def enqueue(self, x):
+    def transfer(self):
         for i in range(self.size1()):
             self.s2.append(self.s1[-1])
             self.s1.pop()
-        self.s1.append(x)
-        for j in range(self.size2()):
-            self.s1.append(self.s2[-1])
-            self.s2.pop()
 
     def dequeue(self):
-        if len(self.s1) == 0:
+        if self.size1() == 0 and self.size2() == 0:
             return None
-        x = self.s1[-1]
-        self.s1.pop()
-        return x
+        if self.size2() == 0:
+            self.transfer()
+        self.s2.pop()

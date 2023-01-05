e0f0461654fd26eb71c1f3d6a6c632f40831073e
Evgeniy P <genepastukhov@gmail.com>
2023-01-05 19:51:03 +0300

    powerset final

--- algotask10.py
+++ algotask10.py
@@ -1,39 +1,15 @@
 class PowerSet:
 
     def __init__(self):
-        self.size1 = 15
-        self.step = 3
-        self.slots = [None] * self.size1
+        self.slots = []
 
     def size(self):
-        count = 0
-        for i in self.slots:
-            if i is not None:
-                count += 1
-        return count
-
-    def hash_fun(self, value: str):
-        x = 0
-        for i in value:
-            x += ord(i)
-        x = x % self.size1
-        return x
+        return len(self.slots)
 
     def put(self, value):
-        indx = self.hash_fun(value)
-        count = 0
-        if value in self.slots:
+        if self.get(value) is True:
             return None
-        while None in self.slots:
-            if self.slots[indx] is None:
-                self.slots[indx] = value
-                return None
-            if self.slots[indx] is not None:
-                indx += self.step
-            if indx > self.size1 - 1:
-                indx = count
-                count += 1
-        return None
+        self.slots.append(value)
 
     def get(self, value):
         if value in self.slots:
@@ -42,39 +18,40 @@ class PowerSet:
 
     def remove(self, value):
         if self.get(value) is True:
-            indx = self.hash_fun(value)
-            self.slots[indx] = None
+            self.slots.remove(value)
             return True
         return False
 
     def intersection(self, set2):
-        lst = []
-        for i in self.slots:
-            if i in set2:
-                lst.append(i)
-        result = set(lst)
-        return result
+        set1 = PowerSet()
+        for i in set2.slots:
+            if self.get(i) is True:
+                set1.put(i)
+        return set1
 
     def union(self, set2):
-        for i in set2:
-            if i in self.slots:
-                continue
-            self.put(i)
-        return self.slots
+        set1 = PowerSet()
+        if len(set2.slots) == 0 and self.size() == 0:
+            return set1
+        for i in self.slots:
+            set1.put(i)
+        for j in set2.slots:
+            set1.put(j)
+        return set1
 
     def difference(self, set2):
-        lst = []
+        set1 = PowerSet()
         for i in self.slots:
-            if i not in set2 and i is not None:
-                lst.append(i)
-        result = set(lst)
-        return result
+            if set2.get(i) is False:
+                set1.put(i)
+        return set1
 
     def issubset(self, set2):
         count = 0
-        for i in set2:
-            if i in self.slots and i is not None:
-                count += 1
-        if count == len(set2):
+        for i in self.slots:
+            for j in set2.slots:
+                if i == j:
+                    count += 1
+        if count == len(set2.slots):
             return True
         return False

8179af1e26420db0d059d1cc3cac10f1123c1b40
Evgeniy P <genepastukhov@gmail.com>
2023-02-04 23:40:21 +0300

    list of names

---
+++ clean_code_names.py
@@ -0,0 +1,12 @@
+1. distance - total_distance_km // общее пройденное расстояние в км
+2. sum - total_amount_territory // общее кол-во территорий
+3. check - boolean_flag // флажок для выхода из цикла
+4. salary1 - sorted_list_salary_desc // список отсортированный зарплат по убыванию
+5. next - next_lock_code // переменная хранящая следующий код замка
+6. length - length_street1 // длина улицы номер 1
+7. x1 - string_less_10charact // строка где меньше 10 букв
+8. sum - sum_all_values // сумма всех значений списка
+9. s - string_no_space // строка без пробелов между слов
+10. result - total_costs_per_month // общее кол-во костов в месяц
+11. n1 -  length_id_client // длина идентификационного номера клиента
+12. maxperc - maximum_percent_candidate // переменная хранящая максимальный процент голосов у кандидата

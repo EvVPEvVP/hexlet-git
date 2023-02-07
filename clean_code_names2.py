357479542ae0df1d09ad695d94816002539cae08
Evgeniy P <genepastukhov@gmail.com>
2023-02-08 01:56:17 +0300

    clean_code_names2

---
+++ clean_code_names2.py
@@ -0,0 +1,28 @@
+6.1 
+salary1 - sorted_list_salary_desc // список отсортированный зарплат по убыванию
+redospisok - list_redo_elements // хранит список возвратных элементов
+delspisok - list_delete_elements // хранит список удаленных элементов
+price - sorted_total_purchases // все отсортированные покупки
+count - amount_dots_in_str // кол-во точек в строке
+x - length_of_the_room // длина комнаты
+
+6.2
+boolean_flag // флажок для выхода из цикла
+binary_data // список с 1 и 0
+cache_size // хранит размер кэша
+hash_key // хранит хэш ключ
+
+6.3
+human_name, human_age; // имя и возраст применяется к человеку
+car_year, car_model; // год и модель применяется к автомобилю
+movie_director, movie_year // директор (режиссер) и год к фильму
+
+6.4
+count - count_in_loop // счетчик в цикле
+diff - length_max_minus_min // разница между макс и мин значением
+mat2 - list_store_values // список хранимых значений
+curr - current_element // хранит текущий элемент
+prev - previous_element // хранит предыдущий элемент
+
+
+

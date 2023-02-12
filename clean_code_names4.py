ba017768d5937528944c0b9189daa830f596b8c3
Evgeniy P <genepastukhov@gmail.com>
2023-02-12 17:07:38 +0300

    avoidable names

---
+++ clean_code_names4.py
@@ -0,0 +1,12 @@
+fact - nuts_squirrel
+distance - total_distance_km
+L1 - captured_areas
+x3 - temp_list_days
+result - final_sorted_salary
+salary1 - temp_salary_list
+sum = sum_of_sequences
+length  - length_points_km
+x2 - split_substring
+result - binary_final_list
+n1 = length_of_input_str1 ; n2 = length_of_input_str2
+diff - diff_between_len_str

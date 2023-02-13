89a5a260ef70b07959e6c7da001050bade776ffc
Evgeniy P <genepastukhov@gmail.com>
2023-02-13 23:29:40 +0300

    correct names of classes

---
+++ clean_code_classes1.py
@@ -0,0 +1,17 @@
+3.1
+databaseConnect - Database
+getData - DataCollector
+processFile - FileProcessor
+readText - TextReader
+searchString - StringSearcher
+
+3.2
+Cat and Dog - classes have methods named pounce() and leap() // the common name: leap
+Squirrel and Raccoon - classes have methods named climb() and climb up() // climb
+Rocket and Plane -  classes have methods named ascend() and soar() // soar
+Engine and Generator -  classes have methods named generate_power() and produce_energy() // produce
+Battery and Solar Panel -  classes  have methods named store_energy() and harvest_energy() // storing
+Compass and GPS -  classes  have methods named determine_direction() and locate() // locate
+Electric_Appliance and Mechanical_Device -  classes have methods named power_on() and activate() // activate
+Ballistic_System and Guided_System -  classes  have methods named launch() and fire() // launch
+

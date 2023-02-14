55a69ca2fcb3ccfe59e859b1341f4d4e51ff0ed1
Evgeniy P <genepastukhov@gmail.com>
2023-02-14 22:38:46 +0300

    name of methods/functions

---
+++ clean_code_methods1.py
@@ -0,0 +1,13 @@
+SynchronizingTables - return_salary_by_ID // return salary by employee ID.
+MaximumDiscount - get_maximum_discount // get maximum discount from list.
+MatrixTurn - turn_the_matrix //  turn the matrix.
+name - get_name // class dog; return the dog name.
+total - get_balance_usd // class bankdeposit; return total usd from the bank account.
+introduce - introduce_yourself // class person; person introduce yourself.
+totalkm - read_odometer // class car; return the value of odometer.
+transfer - transf_usd_to_invest_acc // class bankdeposit; transfer USD to investment account from payment account.
+total - get_area // class rectangle; return rectangle area.
+telephone - get_telephone_number // class person; return telephone number.
+milkprice - change_the_milk_price // class grocery; change the price of milk.
+checkthetable - find_value_in_table // find the value in the table.
+

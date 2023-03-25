42c991a836c0f109da20791c53fd8645f6898f17
Evgeniy P <genepastukhov@gmail.com>
2023-03-25 18:20:35 +0300

    widhtdeep

--- bst2.py
+++ bst2.py
@@ -42,9 +42,7 @@ class BST:
         return tuple(result)
 
     def DeepAllNodes(self, order):
-        """
-        0 (in-order), 1 (post-order), or 2 (pre-order).
-        """
+        
         result = []
 
         def traverse(node):

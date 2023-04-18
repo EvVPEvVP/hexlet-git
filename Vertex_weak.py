c9b1cfc99c4e80cd82d6428dc81192c6a40d9f77
Evgeniy P <genepastukhov@gmail.com>
2023-04-18 22:42:01 +0300

    vertex weak

--- Vertex_weak.py
+++ Vertex_weak.py
@@ -95,37 +95,3 @@ class SimpleGraph:
         return weak_vertices
 
 
-
-def test_WeakVertices():
-    # Create a simple graph with 6 vertices and add edges to form 2 triangles
-    g = SimpleGraph(9)
-    g.AddVertex(0)
-    g.AddVertex(1)
-    g.AddVertex(2)
-    g.AddVertex(3)
-    g.AddVertex(4)
-    g.AddVertex(5)
-    g.AddVertex(6)
-    g.AddVertex(7)
-    g.AddVertex(8)
-    g.AddEdge(0, 1)
-    g.AddEdge(0, 2)
-    g.AddEdge(0, 3)
-    g.AddEdge(1, 3)
-    g.AddEdge(2, 3)
-    g.AddEdge(1, 4)
-    g.AddEdge(3, 5)
-    g.AddEdge(4, 5)
-    g.AddEdge(5, 7)
-    g.AddEdge(5, 8)
-    g.AddEdge(7, 8)
-    g.AddEdge(6, 8)
-    # g.AddEdge(5, 6)
-    # g.AddEdge(4, 7)
-
-    # Test WeakVertices method
-    weak_vertices = g.WeakVertices()
-    print(weak_vertices)
-
-test_WeakVertices()
-

03411f0031bb4a4b64cf17c36410c57308ac52a6
EvVPEvVP <117113293+EvVPEvVP@users.noreply.github.com>
2024-10-06 15:16:10 +0300

    Add files via upload

---
+++ 2.f.2(1).py
@@ -0,0 +1,27 @@
+2.3.1.
+
+from pymonad.tools import curry
+
+@curry(2)
+def concat_strings(s1, s2):
+    return s1 + s2
+
+hello_name = concat_strings("Hello, ")
+
+print(hello_name("John"))  # Выведет: Hello, John
+
+
+2.3.2.
+
+from pymonad.tools import curry
+
+@curry(4)
+def greet(greeting, punctuation, name, end_sign):
+    return f"{greeting}{punctuation} {name}{end_sign}"
+
+first_step = greet("Hello")(",")("!")
+result = first_step("Petya")
+print(result)  # Вывод: Hello, Petya!
+
+# Другой вариант использования
+print(greet("Hi")(":")("!!!")("Alice"))  # Выведет: Hi: Alice!!!
